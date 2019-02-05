b76a3592c618b38f0181c02580d85688a8df4c5d
Mukulika Dey <muku.dey27@gmail.com>
2019-02-05 00:24:34 -0500

    Added proper documentation for methods

--- queries.py
+++ queries.py
@@ -4,26 +4,52 @@ from difflib import SequenceMatcher
 from operator import itemgetter
 
 
-# load our search index
 def get_index():
+    """
+    This loads our search index.
+
+    :return: a list of cities as dictionary objects.
+    """
     with open('search_indexer.json', 'r') as file:
         return json.load(file)
 
 
-# gets the score for names
 def get_score_name(query_name, actual_name):
+    """
+    This gets the score for name queries.
+
+    :param query_name: the query name that the user inputs.
+    :param actual_name: the city name in our index.
+    :return: similarity score between the query and actual name.
+    """
     return SequenceMatcher(lambda x: x == " ", actual_name, query_name).ratio()
 
 
-# gets the score for numeric query parameters
 def get_score_number(query_num, actual_num, max_num):
+    """
+    This gets the score for numeric query parameters.
+
+    :param query_num: the numeric query that the user inputs.
+    :param actual_num: the city's numeric value in our index - can be lat, long, or pop.
+    :param max_num: the maximum value for that numeric property.
+    :return: the normalized score between the query and actual name.
+    """
     score = (1 - abs(actual_num - query_num) / max_num) if (max_num != 0) else 0
 
     return score
 
 
-# gets sorted results based on query parameters
 def get_results(name, population, lat, long, cities):
+    """
+    This gets the sorted results based on query parameters.
+
+    :param name: the query name that the user inputs.
+    :param population: the query population that the user inputs.
+    :param lat: the query latitude that the user inputs.
+    :param long: the query longitude that the user inputs.
+    :param cities: the list of cities in our index.
+    :return: the relevant results based on user query in decreasing order of scores.
+    """
     results = []
 
     max_pop = 0
